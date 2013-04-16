75aef0ce21224cb9d8033666a00a28ccf7513a7b
Saravanan Ganesh <srrvnn@live.com>
2013-04-16 15:35:04 -0700

    added index file to use WebCrawler.java

---
+++ code/WebCrawler/src/webcrawler/index.java
@@ -0,0 +1,14 @@
+
+// package webcrawler;
+
+import java.lang.Exception;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class index {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
+
+		WebCrawler ocrawler = new WebCrawler();
+	}
+}
